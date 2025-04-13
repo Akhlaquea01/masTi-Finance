f386ed294eeaf513fbd317132c7af6d2540e9214
Akhlaque <akhlaquea01@gmail.com>
2025-04-13 15:53:31 +0530

    feat: implement toolbar with buttons and avatar, update dark mode settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import { AvatarModule } from 'primeng/avatar';
 @Component({
   selector: 'masTi-root',
-  imports: [ButtonModule],
+  imports: [ButtonModule, ToolbarModule, AvatarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
